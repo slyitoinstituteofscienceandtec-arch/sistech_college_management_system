fef2a55a7719cc94b7f33bae137b12ad75d1bb71
SISTECH <sistech2025@gmail.com>
2026-07-20 01:04:27 +0000

    Move file_url helper to AppServiceProvider for reliable loading on Render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (!function_exists('file_url')) {
+            function file_url(?string $path): string
+            {
+                if ($path && (str_starts_with($path, 'http://') || str_starts_with($path, 'https://'))) {
+                    return $path;
+                }
+
+                return asset('storage/' . ($path ?? ''));
+            }
+        }
     }
 
     /**
